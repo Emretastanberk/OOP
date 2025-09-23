2b0f22578e55359a506316f225af931464cd63f4
Emre Taştanberk <24301071009@kayseri.edu.tr>
2025-09-23 15:13:18 +0300

    Updated

---
+++ OOP2526/Lesson1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Islem t = new Islem();
+            t.topla();
+            t.kalan();
+
+            //int kalan = a % b;
+            //if (kalan == 0)
+            //    Console.WriteLine("Bu sayılar tam bölünür");
+            //else
+            //    Console.WriteLine("Bu sayılar tam bölünmez");
+        }
+
+        class Islem
+        {
+            public int topla()
+            {
+                Console.Write("A sayısını girin: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("B sayısını girin: ");
+                int b = int.Parse(Console.ReadLine());
+                int toplam = a + b;
+                Console.WriteLine("Toplam: " + toplam);
+                return toplam;
+            }
+
+            public bool kalan()
+            {
+                Console.Write("A sayısını girin: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("B sayısını girin: ");
+                int b = int.Parse(Console.ReadLine());
+                int kalan_ = a % b;
+                if (kalan_ == 0)
+                {
+                    Console.WriteLine("Bu sayılar tam bölünür");
+                    return true;
+                }
+                    
+                else
+                {
+                    Console.WriteLine("Bu sayılar tam bölünmez");
+                    return false;
+                }
+                    
+            }
+        }
+    }
+}
