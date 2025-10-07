011300622db494a1e60a202cf7bebe353e1fd673
Emre Taştanberk <24301071009@kayseri.edu.tr>
2025-10-07 10:55:36 +0300

    Updated

--- OOP2526/Lesson2/Program.cs
+++ OOP2526/Lesson2/Program.cs
@@ -29,21 +29,63 @@ namespace Lesson2
             //    Console.WriteLine(sayi);
             //}
 
-            int a = 10;
-            int b = 200;
-            int c = 110;
+            //int a = 10;
+            //int b = 200;
+            //int c = 110;
 
-            int enBuyuk = a;
+            //int enBuyuk = a;
 
-            if (b > enBuyuk)
-                enBuyuk = b;
-            if (c > enBuyuk)
-                enBuyuk = c;
+            //if (b > enBuyuk)
+            //    enBuyuk = b;
+            //if (c > enBuyuk)
+            //    enBuyuk = c;
 
-            Console.WriteLine("En büyük sayı: " + enBuyuk);
-        
+            //Console.WriteLine("En büyük sayı: " + enBuyuk);
 
-    }
+            //Console.WriteLine("1. Kenarı giriniz: ");
+            //int kenar1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("2. Kenarı giriniz: ");
+            //int kenar2 = int.Parse(Console.ReadLine());
+
+            //int pisagor = (kenar1 * kenar1) + (kenar2 * kenar2);
+            //double sonuc = Math.Sqrt(pisagor);
+            //Console.WriteLine("Hipotenüs: " + sonuc);
+             
+            //Console.WriteLine("Bir sayı girin: ");
+            //int sayi = int.Parse(Console.ReadLine());
+
+            //switch (sayi)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Pazartesi");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Salı");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Çarşamba");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Perşembe");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Cuma");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Cumartesi");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Pazar");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Geçersiz sayı girdiniz.");
+            //        break;
+
+
+            }
+
+        }
 
 
     }
---
+++ OOP2526/Lesson3/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EkranaYaz();
+            //OgrNo(24301071009);
+            //IsimSoyisim("Emre", "Taştanberk");
+            //Ogrenci(1, "Emre");
+            //Ogrenci ogr1 = new Ogrenci();
+            //ogr1.numara = 24301071009;
+            //ogr1.isim = "Emre";
+            //ogr1.soyisim = "Taştanberk";
+            //ogr1.not = 95.5;
+            //ogr1.BilgileriYazdir();
+            //Islem sayiAl = new Islem();
+            //sayiAl.sayiAl();
+
+            //Islem topla = new Islem();
+            //topla.sayi1 = sayiAl.sayi1;
+            //topla.sayi2 = sayiAl.sayi2;
+            //Console.WriteLine("Toplama: " + topla.Topla());
+            BuyukSayiBul(15, 10, 3);
+            
+
+
+        }
+
+        static int BuyukSayiBul(int sayi1, int sayi2, int sayi3)
+        {
+            int buyukSayi = sayi1;
+            if (sayi2 > buyukSayi)
+            {
+                buyukSayi = sayi2;
+            }
+            if (sayi3 > buyukSayi)
+            {
+                buyukSayi = sayi3;
+            }
+            return buyukSayi;
+        }
+
+            //class Islem
+            //{
+            //    public int sayi1;
+            //    public int sayi2;
+
+            //    public int sayiAl()
+            //    {
+            //        Console.Write("Birinci sayıyı giriniz: ");
+            //        sayi1 = Convert.ToInt32(Console.ReadLine());
+            //        Console.Write("İkinci sayıyı giriniz: ");
+            //        sayi2 = Convert.ToInt32(Console.ReadLine());
+            //        return 0;
+            //    }
+            //    public int Topla()
+            //    {
+            //        return sayi1 + sayi2;
+            //    }
+            //    public int Cikar()
+            //    {
+            //        return sayi1 - sayi2;
+            //    }
+            //    public int Carp()
+            //    {
+            //        return sayi1 * sayi2;
+            //    }
+            //    public double Bol()
+            //    {
+            //        return (double)sayi1 / sayi2;
+            //    }
+            //}
+            //class Ogrenci
+            //{
+            //    public double numara;
+            //    public string isim;
+            //    public string soyisim;
+            //    public double not;
+
+            //    public void BilgileriYazdir()
+            //    {
+            //        Console.WriteLine("Numara: " + numara);
+            //        Console.WriteLine("İsim: " + isim);
+            //        Console.WriteLine("Soyisim: " + soyisim);
+            //        Console.WriteLine("Not: " + not);
+            //    }
+            //}
+
+            //static void EkranaYaz()
+            //{
+            //    Console.WriteLine("mesaj");
+            //}
+            //static void Ogrenci(int numara, string isim )
+            //{
+            //    Console.WriteLine(numara +" "+ isim);
+            //}
+            //static void OgrNo(double no)
+            //{
+            //    Console.WriteLine(no);
+            //}
+            //static void IsimSoyisim(string isim, string soyisim)
+            //{
+            //    Console.WriteLine(isim + " " + soyisim);
+            //}
+        }
+}
